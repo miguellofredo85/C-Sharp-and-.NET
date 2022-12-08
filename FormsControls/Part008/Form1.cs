92f484f45916cf1c41b0cfbd710a34a09c9fe004
Miguel <miguellofredo@gmail.com>
2022-12-08 02:22:27 -0300

    adding datetimepicker

---
+++ FormsControls/Part008/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Part008
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("Year "+dateTimePicker1.Value.DayOfWeek);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var date = dateTimePicker1.Value;
+            MessageBox.Show(date.ToString());
+        }
+    }
+}
