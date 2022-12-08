613c2e89f2d21ec4d47136a5cb1be5d95330ea19
Miguel <miguellofredo@gmail.com>
2022-12-08 01:50:38 -0300

    adding exercises check list

--- FormsControls/Part006/Form1.Designer.cs
+++ FormsControls/Part006/Form1.Designer.cs
@@ -28,13 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.checkedListBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.checkedListBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Bicicle",
+            "Boat",
+            "Car",
+            "Helicopter",
+            "Moto",
+            "Plane",
+            "Skate"});
+            this.checkedListBox1.Location = new System.Drawing.Point(10, 10);
+            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(294, 560);
+            this.checkedListBox1.Sorted = true;
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(350, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Click Me!\r\n";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(835, 615);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Font = new System.Drawing.Font("Comic Sans MS", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- FormsControls/Part006/Form1.cs
+++ FormsControls/Part006/Form1.cs
@@ -15,6 +15,31 @@ namespace Part006
         public Form1()
         {
             InitializeComponent();
+            checkedListBox1.Items.Add("Monopatin", true);
+            checkedListBox1.Items.Add("Titanic", false);
+
+            int qtty = checkedListBox1.Items.Count;
+            MessageBox.Show("Total items " + qtty);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // checkedListBox1.Items.Clear();
+            // checkedListBox1.Items.AddRange(new object[] { "Sarlangoso", "Pombadeiro", "Urlingatacambe" });
+            // bool result = checkedListBox1.Items.Contains("Moto");
+            // MessageBox.Show("Yes it's "+result);
+            // checkedListBox1.Items.Remove("Moto");
+            // checkedListBox1.Items.RemoveAt(0);
+            // checkedListBox1.Items.Insert(5, "Horse");
+
+
+            // var ch = checkedListBox1.CheckedItems;
+            var ch = checkedListBox1.SelectedItems;
+            foreach (object item in ch)
+            {
+                MessageBox.Show((string)item);
+            }
+        
         }
     }
 }
