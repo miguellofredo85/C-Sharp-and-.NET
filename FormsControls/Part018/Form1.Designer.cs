3915eb02e3963d34194fd1b4802fe5dfd30d7681
Miguel <miguellofredo@gmail.com>
2022-12-08 05:27:16 -0300

    adding treebox

---
+++ FormsControls/Part018/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Part018
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Warning;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(131, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(563, 88);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Help me...";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.toolTip1.SetToolTip(this.label1, "Help me button!\r\n");
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(356, 245);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.toolTip2.SetToolTip(this.button1, "I will not help you again!");
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // toolTip2
+            // 
+            this.toolTip2.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolTip toolTip2;
+    }
+}
+
---
+++ FormsControls/Part019/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Part019
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MountTree();
+            
+        }
+        private void MountTree()
+        {
+            treeView1.BeginUpdate();
+
+            treeView1.Nodes.Add("Miguel");
+            treeView1.Nodes[0].Nodes.Add("is");
+            treeView1.Nodes[0].Nodes[0].Nodes.Add("the");
+            treeView1.Nodes[0].Nodes[0].Nodes[0].Nodes.Add("best");
+            treeView1.Nodes.Add("Yes!");
+
+
+            treeView1.EndUpdate();
+        }
+    }
+}
