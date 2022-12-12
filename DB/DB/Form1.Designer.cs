85410df65e04dc43384668204548c7d679929ff3
Miguel <miguellofredo@gmail.com>
2022-12-12 14:13:56 -0300

    Adding DB connections, db creation and table and insert resgisters with SQLCe Server, MySQL, SQLite,

---
+++ DB/DB/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace DB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnTable = new System.Windows.Forms.Button();
+            this.btnPut = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.S = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(5, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(735, 86);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Result";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.Name,
+            this.email});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 400);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(723, 378);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Id
+            // 
+            this.Id.HeaderText = "ID";
+            this.Id.MinimumWidth = 6;
+            this.Id.Name = "Id";
+            // 
+            // Name
+            // 
+            this.Name.HeaderText = "NAME";
+            this.Name.MinimumWidth = 6;
+            this.Name.Name = "Name";
+            // 
+            // email
+            // 
+            this.email.HeaderText = "EMAIL";
+            this.email.MinimumWidth = 6;
+            this.email.Name = "email";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 187);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(634, 98);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(36, 285);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(634, 98);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Email";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(152, 194);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(560, 30);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(152, 292);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(560, 30);
+            this.textBox2.TabIndex = 5;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(741, 398);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(257, 46);
+            this.btnConnect.TabIndex = 6;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnTable
+            // 
+            this.btnTable.Location = new System.Drawing.Point(741, 450);
+            this.btnTable.Name = "btnTable";
+            this.btnTable.Size = new System.Drawing.Size(257, 45);
+            this.btnTable.TabIndex = 7;
+            this.btnTable.Text = "Create Table";
+            this.btnTable.UseVisualStyleBackColor = true;
+            this.btnTable.Click += new System.EventHandler(this.btnTable_Click);
+            // 
+            // btnPut
+            // 
+            this.btnPut.Location = new System.Drawing.Point(741, 501);
+            this.btnPut.Name = "btnPut";
+            this.btnPut.Size = new System.Drawing.Size(257, 45);
+            this.btnPut.TabIndex = 8;
+            this.btnPut.Text = "Create";
+            this.btnPut.UseVisualStyleBackColor = true;
+            this.btnPut.Click += new System.EventHandler(this.btnPut_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(741, 552);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(257, 45);
+            this.btnSearch.TabIndex = 9;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(741, 603);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(257, 45);
+            this.btnDelete.TabIndex = 10;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // S
+            // 
+            this.S.Location = new System.Drawing.Point(741, 654);
+            this.S.Name = "S";
+            this.S.Size = new System.Drawing.Size(257, 45);
+            this.S.TabIndex = 11;
+            this.S.Text = "Edit";
+            this.S.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1010, 791);
+            this.Controls.Add(this.S);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnPut);
+            this.Controls.Add(this.btnTable);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn email;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnTable;
+        private System.Windows.Forms.Button btnPut;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button S;
+    }
+}
+
---
+++ DB/DB/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlServerCe; // importing SQL
+using System.Data.SQLite; // imoprting SQLite
+using System.IO; // importing IO for files handling
+using MySql.Data.MySqlClient; // import MySQL 
+using System.Globalization;
+
+namespace DB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            #region SQLServer CE
+            //string dataBase = Application.StartupPath + @"\db\DBSQLServer.sdf";
+            //string connection = @"DataSource = " + dataBase + "; Password = ''";
+            //SqlCeEngine db = new SqlCeEngine(connection);
+
+            //if (!File.Exists(connection))
+            //{
+            //    db.CreateDatabase(); // create
+            //}
+            //db.Dispose(); // close db and free resources
+
+            //SqlCeConnection connect = new SqlCeConnection(connection); // connect 
+            //try
+            //{
+            //    connect.Open(); // open connection
+
+            //    label1.Text = "SQLServerCe connected";
+            //}
+            //catch (Exception ex)
+            //{
+            //    label1.Text = "SQLServerCe not connected\n" + ex;
+            //}
+            //finally
+            //{
+            //    connect.Close();
+            //}
+
+            #endregion
+
+            #region SQLite
+
+
+            //string dataBase = Application.StartupPath + @"\db\DBSQLite.sdf";
+            //string connection = @"DataSource = " + dataBase + "; Version = 3";
+
+            //if (!File.Exists(dataBase))
+            //{
+            //    SQLiteConnection.CreateFile(dataBase); // create
+            //}
+
+            //SQLiteConnection connect = new SQLiteConnection(connection); // connect 
+            //try
+            //{
+            //    connect.Open(); // open connection
+
+            //    label1.Text = "SQLite connected";
+            //}
+            //catch (Exception ex)
+            //{
+            //    label1.Text = "SQLite not connected\n" + ex;
+            //}
+            //finally
+            //{
+            //    connect.Close();
+            #endregion
+
+            #region MySQL
+            string strConnection = "server=127.0.0.1; User Id=root ;password=";
+            MySqlConnection connect= new MySqlConnection(strConnection);
+
+
+            try
+            {
+                connect.Open(); // open connection
+
+                label1.Text = "MySQL connected";
+
+                MySqlCommand comand= new MySqlCommand();
+                comand.Connection = connect;
+                comand.CommandText = "CREATE DATABASE IF NOT EXISTS course_db";
+
+                comand.ExecuteNonQuery();
+                label1.Text = "DataBase course_db created!";
+                comand.Dispose();
+
+            }
+            catch (Exception ex)
+            {
+                label1.Text = "MySQL not connected\n" + ex;
+            }
+            finally
+            {
+                connect.Close();
+
+            #endregion
+        }
+    }
+
+        private void btnTable_Click(object sender, EventArgs e)
+        {
+            #region SQL Server
+            //string dataBase = Application.StartupPath + @"\db\DBSQLServer.sdf";
+            //string connection = @"DataSource = " + dataBase + "; Password = ''";
+            //SqlCeConnection connect= new SqlCeConnection(connection);
+
+            //try
+            //{
+            //    connect.Open();
+
+            //    SqlCeCommand command= new SqlCeCommand();
+            //    command.Connection = connect;
+
+            //    command.CommandText = "CREATE TABLE persons(id INT NOT NULL PRIMARY KEY, name NVARCHAR(50), email NVARCHAR(50))";
+            //    command.ExecuteNonQuery();
+
+            //    label1.Text = "Table persons created (SQL Server) !";
+
+            //    command.Dispose();
+            //}
+            //catch(Exception ex)
+            //{
+            //    label1.Text = ex.Message;
+            //}
+            //finally
+            //{
+            //    connect.Close();
+            //}
+            #endregion
+
+            // to SQLite is the same code, just change SqlCe
+
+            #region MySQL
+
+            
+            string connection = "Server=127.0.0.1; User Id=root;database=course_db;password=";
+            MySqlConnection connect = new MySqlConnection(connection);
+
+            try
+            {
+                connect.Open();
+
+                MySqlCommand command = new MySqlCommand();
+                command.Connection = connect;
+
+                command.CommandText = "CREATE TABLE persons(id INT NOT NULL, name NVARCHAR(50), email NVARCHAR(50), PRIMARY KEY(id))";
+                command.ExecuteNonQuery();
+
+                label1.Text = "Table persons created (SQL Server) !";
+
+                command.Dispose();
+            }
+            catch (Exception ex)
+            {
+                label1.Text = ex.Message;
+            }
+            finally
+            {
+                connect.Close();
+            }
+
+            #endregion
+        }
+
+        private void btnPut_Click(object sender, EventArgs e)
+        {
+            #region SQL Server
+            //string dataBase = Application.StartupPath + @"\db\DBSQLServer.sdf";
+            //string connection = @"DataSource = " + dataBase + "; Password = ''";
+            //SqlCeConnection connect = new SqlCeConnection(connection);
+
+            //try
+            //{
+            //    connect.Open();
+
+            //    SqlCeCommand command = new SqlCeCommand();
+            //    command.Connection = connect;
+
+            //    int id = new Random(DateTime.Now.Millisecond).Next(0, 1000);
+            //    string name = textBox1.Text;
+            //    string email = textBox2.Text;
+
+            //    command.CommandText = "INSERT INTO persons VALUES ("+ id +", '"+ name +"', '"+email+"')";
+
+            //    command.ExecuteNonQuery();
+
+            //    label1.Text = "Register created (SQL Server) !";
+
+            //    command.Dispose();
+            //}
+            //catch (Exception ex)
+            //{
+            //    label1.Text = ex.Message;
+            //}
+            //finally
+            //{
+            //    connect.Close();
+            //}
+            #endregion
+
+            // to SQLite is the same code, just change SqlCe 
+
+
+            #region MySQL
+            string connection = "Server=127.0.0.1; User Id=root;database=course_db;password=";
+            MySqlConnection connect = new MySqlConnection(connection);
+
+            try
+            {
+                connect.Open();
+
+                MySqlCommand command = new MySqlCommand();
+                command.Connection = connect;
+
+                int id = new Random(DateTime.Now.Millisecond).Next(0, 1000);
+                string name = textBox1.Text;
+                string email = textBox2.Text;
+
+                command.CommandText = "INSERT INTO persons VALUES (" + id + ", '" + name + "', '" + email + "')";
+
+                command.ExecuteNonQuery();
+
+                label1.Text = "Register created (MySQL)!";
+
+                command.Dispose();
+            }
+            catch (Exception ex)
+            {
+                label1.Text = ex.Message;
+            }
+            finally
+            {
+                connect.Close();
+            }
+            #endregion
+
+        }
+    }
+}
