13366da8180f5451672cfcc1206d8d53fa2001e6
Miguel <miguellofredo@gmail.com>
2022-12-14 06:23:59 -0300

    adding LINQ - Language Integrated Query with and without Lambda

---
+++ LINQ_1/LINQ_1/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace LINQ_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.btnWhere = new System.Windows.Forms.Button();
+            this.btnOrderBy = new System.Windows.Forms.Button();
+            this.btnGroupBy = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnElement = new System.Windows.Forms.Button();
+            this.btnLinqLamda = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox
+            // 
+            this.listBox.Font = new System.Drawing.Font("Comic Sans MS", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 38;
+            this.listBox.Location = new System.Drawing.Point(0, 0);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(618, 536);
+            this.listBox.TabIndex = 0;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(624, 0);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(425, 34);
+            this.txtSearch.TabIndex = 1;
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(745, 55);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(144, 55);
+            this.btnExecute.TabIndex = 2;
+            this.btnExecute.Text = "Search";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // btnWhere
+            // 
+            this.btnWhere.Location = new System.Drawing.Point(745, 116);
+            this.btnWhere.Name = "btnWhere";
+            this.btnWhere.Size = new System.Drawing.Size(144, 56);
+            this.btnWhere.TabIndex = 3;
+            this.btnWhere.Text = "Where";
+            this.btnWhere.UseVisualStyleBackColor = true;
+            this.btnWhere.Click += new System.EventHandler(this.btnWhere_Click);
+            // 
+            // btnOrderBy
+            // 
+            this.btnOrderBy.Location = new System.Drawing.Point(745, 178);
+            this.btnOrderBy.Name = "btnOrderBy";
+            this.btnOrderBy.Size = new System.Drawing.Size(144, 56);
+            this.btnOrderBy.TabIndex = 4;
+            this.btnOrderBy.Text = "Order By...";
+            this.btnOrderBy.UseVisualStyleBackColor = true;
+            this.btnOrderBy.Click += new System.EventHandler(this.btnOrderBy_Click);
+            // 
+            // btnGroupBy
+            // 
+            this.btnGroupBy.Location = new System.Drawing.Point(745, 240);
+            this.btnGroupBy.Name = "btnGroupBy";
+            this.btnGroupBy.Size = new System.Drawing.Size(144, 56);
+            this.btnGroupBy.TabIndex = 5;
+            this.btnGroupBy.Text = "Group By...";
+            this.btnGroupBy.UseVisualStyleBackColor = true;
+            this.btnGroupBy.Click += new System.EventHandler(this.btnGroupBy_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(745, 302);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(144, 56);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "Agregator Operator";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnElement
+            // 
+            this.btnElement.Location = new System.Drawing.Point(745, 364);
+            this.btnElement.Name = "btnElement";
+            this.btnElement.Size = new System.Drawing.Size(144, 56);
+            this.btnElement.TabIndex = 7;
+            this.btnElement.Text = "Element Operator";
+            this.btnElement.UseVisualStyleBackColor = true;
+            this.btnElement.Click += new System.EventHandler(this.btnElement_Click);
+            // 
+            // btnLinqLamda
+            // 
+            this.btnLinqLamda.Location = new System.Drawing.Point(745, 426);
+            this.btnLinqLamda.Name = "btnLinqLamda";
+            this.btnLinqLamda.Size = new System.Drawing.Size(144, 56);
+            this.btnLinqLamda.TabIndex = 8;
+            this.btnLinqLamda.Text = "Linq Lambda";
+            this.btnLinqLamda.UseVisualStyleBackColor = true;
+            this.btnLinqLamda.Click += new System.EventHandler(this.btnLinqLamda_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1065, 551);
+            this.Controls.Add(this.btnLinqLamda);
+            this.Controls.Add(this.btnElement);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnGroupBy);
+            this.Controls.Add(this.btnOrderBy);
+            this.Controls.Add(this.btnWhere);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.listBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.Button btnWhere;
+        private System.Windows.Forms.Button btnOrderBy;
+        private System.Windows.Forms.Button btnGroupBy;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnElement;
+        private System.Windows.Forms.Button btnLinqLamda;
+    }
+}
+
---
+++ LINQ_1/LINQ_1/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LINQ_1
+{
+    public partial class Form1 : Form
+    {
+        List<string> list_nomes;
+        List<int> numbers;
+        Dictionary<string, double> products;
+        Dictionary<string, string> list_status;
+        public Form1()
+        {
+            InitializeComponent();
+            // data 
+            #region list_nomes
+            list_nomes = new List<string>();
+            list_nomes.Add("Miguel");
+            list_nomes.Add("Charles");
+            list_nomes.Add("Arthur");
+            list_nomes.Add("Louise");
+            list_nomes.Add("Lucy");
+            list_nomes.Add("Monic");
+            list_nomes.Add("Annabelle");
+            list_nomes.Add("Ailin");
+            list_nomes.Add("Gabriel");
+            list_nomes.Add("Micaela");
+            list_nomes.Add("Valente");
+            list_nomes.Add("Messi");
+            list_nomes.Add("Maradona");
+            #endregion
+
+            #region numbers
+            numbers = new List<int>();
+            numbers.Add(12);
+            numbers.Add(8);
+            numbers.Add(21);
+            numbers.Add(2);
+            numbers.Add(43);
+            numbers.Add(87);
+            numbers.Add(4);
+            numbers.Add(17);
+            numbers.Add(32);
+            numbers.Add(10);
+            #endregion
+
+            #region products
+
+            products= new Dictionary<string, double>();
+            products.Add("Keyboard", 56.23);
+            products.Add("Mouse", 23.12);
+            products.Add("Tv", 567.12);
+            products.Add("Notebook", 1000.65);
+            products.Add("Memory 8gb", 35.78);
+            products.Add("Motherboard", 543.67);
+            products.Add("PC ATX", 200.89);
+            products.Add("USB", 23.43);
+            products.Add("Playstation", 2000.54);
+            products.Add("Xbox", 1800.45);
+            products.Add("Nintendo", 33.50);
+
+            #endregion
+
+            #region list_states
+            list_status = new Dictionary<string, string>();
+            list_status.Add("Bs As", "Argentina");
+            list_status.Add("Rio de Janeiro", "Brazil");
+            list_status.Add("Sao Paulo", "Brazil");
+            list_status.Add("Cuzco", "Peru");
+            list_status.Add("Mendoza", "Argentina");
+            list_status.Add("Bogota", "Colombia");
+            list_status.Add("Lima", "Peru");
+            list_status.Add("Medellin", "Colombia");
+            list_status.Add("Chaco", "Argentina");
+            #endregion
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Clear();
+            
+            ////LINQ (get data, make a query, execute query)
+            
+            //IEnumerable<int> result = from num in numbers where num % 2 == 0 select num; // query
+            //// this sentence is better than forech because wont go through all list, just evens
+            //// can use var but IEnumerable woulb "correct"
+
+            //foreach (int num in result)
+            //{
+
+            //    listBox.Items.Add(num);
+            //}
+            string txt = txtSearch.Text;
+            IEnumerable<string> result = from name in list_nomes 
+                                         where name.StartsWith(txt) 
+                                         select name;
+            listBox.Items.AddRange(result.ToArray()); // 
+            //foreach (string item in result)
+            //{
+            //    listBox.Items.Add(item);
+            //}
+
+        }
+
+        private void btnWhere_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Clear();
+            // filter where
+            string txt = txtSearch.Text.ToLower();
+            var result = from nome in list_nomes 
+                         where nome.ToLower().Contains(txt) 
+                         select nome;
+
+            foreach (var n in result)
+            {
+                listBox.Items.Add(n);
+            }
+        }
+
+        private void btnOrderBy_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Clear();
+
+            string txt = txtSearch.Text;
+
+            // var result = from num in numbers orderby num descending where num <= 10 select num;
+            // var result = from num in list_nomes orderby num ascending select num;
+            var result = from prod in products  orderby prod.Key descending select prod;
+
+            foreach (KeyValuePair<string, double> n in result)
+            {
+                listBox.Items.Add(n.Key + " R$ " + n.Value);
+            }
+        }
+
+        private void btnGroupBy_Click(object sender, EventArgs e)
+        {
+            // groupby filter
+
+            listBox.Items.Clear();
+
+            string txt = txtSearch.Text;
+
+            var result = from state in list_status 
+                         group state 
+                         by state.Value;
+            foreach (var group in result)
+            {
+                listBox.Items.Add(group.Key); // item.Key cause Key is from var result
+                    foreach (var city in group)
+                {
+                    listBox.Items.Add("   " + city.Key);
+                }          
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Clear();
+
+            //int count1 = (from name in list_nomes where name.StartsWith("G") select name).Count();
+
+            //listBox.Items.Add(count1 + " names stating with G");
+
+            //double average = numbers.Average();
+            //listBox.Items.Add(average);
+
+            //var query = from num in numbers where num <= 10 select num;
+            //var result = query.Average();
+            //listBox.Items.Add(result);
+
+            //int sum = numbers.Sum();
+            //listBox.Items.Add(sum + " sum numbers"); 
+
+            string maxlenght = list_nomes.Aggregate((max, min) => {
+
+                if (max.ToString().Length > min.ToString().Length)
+                {
+                    return max;
+                }
+                else 
+                { 
+                    return min;
+                }
+            
+            });
+
+            listBox.Items.Add(maxlenght + " max lenght name");
+        }
+
+        private void btnElement_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Clear();
+            int first = numbers.FirstOrDefault(); // OrDefault is cause if list number is empty it return 0
+            listBox.Items.Add(first);
+
+            int last = numbers.LastOrDefault(); 
+            listBox.Items.Add(last);
+
+            int index = numbers.ElementAtOrDefault(4);
+            listBox.Items.Add(index);
+        
+            var consult = from num in numbers where num > 1000 select num;
+            listBox.Items.Add(consult.FirstOrDefault());
+        }
+
+        private void btnLinqLamda_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Clear();
+            ////var query = from name in list_nomes select name;
+            //var queryLambda = list_nomes.Select(name => name); // query with lambda
+            //listBox.Items.AddRange(queryLambda.ToArray());
+
+            //var query = from name in list_nomes where name.StartsWith("M") select name;
+            //var queryLambda = list_nomes.Where(name=> name.StartsWith("M")); // query with lambda
+            //listBox.Items.AddRange(queryLambda.ToArray());
+
+            //var query = from name in list_nomes orderby name select name;
+            ////var queryLambda = list_nomes.OrderBy(name => name);
+            //var queryLambda = list_nomes.OrderByDescending(name => name);
+            //listBox.Items.AddRange(queryLambda.ToArray());
+
+            //var query = from state in list_status group state by state.Value;
+            var queryLambda = list_status.GroupBy(state => state.Value);
+
+            foreach ( var item in queryLambda )
+            {
+                listBox.Items.Add(item.Key);
+                    foreach ( var item2 in item) 
+                    { 
+                        listBox.Items.Add("    " + item2.Key);
+                    }
+            }
+        
+        }
+    }
+}
+
+
