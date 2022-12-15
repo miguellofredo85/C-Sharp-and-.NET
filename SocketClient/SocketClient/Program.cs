8ffa3e9fe956b346a475167e15719d4cf62306d8
Miguel <miguellofredo@gmail.com>
2022-12-15 17:26:04 -0300

    Add working with Sockets, connection from client to server and sending text info and implement try-catch error connection

---
+++ SocketClient/SocketClient/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            // InterNetwork is IPv4 | Stream is out -in data
+
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+
+            try
+            {
+                socket.Connect(endPoint);
+                Console.WriteLine("Connection Success");
+                Console.Write("Write info: ");
+
+                string info = Console.ReadLine();
+
+                byte[] sendInfo = Encoding.Default.GetBytes(info);
+
+                socket.Send(sendInfo);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error Connection");
+            }
+
+            socket.Close();
+
+            Console.WriteLine("Press a key to exit...");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SocketServer/SocketServer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket= new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            // InterNetwork is IPv4 | Stream is out -in data
+
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+            
+            socket.Bind(endPoint);
+
+            socket.Listen(5);
+
+            Console.WriteLine("Listening");
+
+            Socket listen = socket.Accept();
+
+            byte[] bytes = new byte[255];
+
+            int buffer = listen.Receive(bytes, 0, bytes.Length, SocketFlags.None);
+
+            Array.Resize<byte>(ref bytes, buffer);
+
+            Console.WriteLine("Client said: ");
+            Console.WriteLine(Encoding.Default.GetString(bytes));
+
+            socket.Close();
+
+            Console.WriteLine("Server finish\n Press a key to exit...");
+
+            Console.ReadKey();
+        }
+    }
+}
