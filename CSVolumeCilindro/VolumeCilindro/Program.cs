9f28d6d6efe74a16b473aa755dff2693a1c2b8fe
Miguel <miguellofredo@gmail.com>
2022-12-21 14:59:33 -0300

    Adding exercise Volume Cilinder

---
+++ CSVolumeCilindro/VolumeCilindro/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace VolumeCilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double R, A, h, V;
+
+            R = 0;
+            while (R <= 0)
+            {
+                Console.Write("Digite o valor do raio da base do cilindro: ");
+                R = double.Parse(Console.ReadLine());
+            }
+
+            h = 0;
+            while (h <= 0)
+            {
+                Console.Write("Digite o valor da altura do cilindro: ");
+                h = double.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+            //área da base
+            A = Math.PI * Math.Pow(R, 2);
+
+            //volume do cilindro
+            V = A * h;
+            Console.WriteLine($"Volume do cilindro {V}");
+        }
+    }
+}
