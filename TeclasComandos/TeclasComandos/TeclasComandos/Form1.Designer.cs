cca55c04a1943a0d0f71266785b0353d99cb3f8d
Miguel <miguellofredo@gmail.com>
2022-12-21 15:00:56 -0300

    Adding Capture Key

---
+++ TeclasComandos/TeclasComandos/TeclasComandos/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace TeclasComandos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.btnX = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(775, 257);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Digite uma tecla...";
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnter.Location = new System.Drawing.Point(570, 371);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(218, 67);
+            this.btnEnter.TabIndex = 4;
+            this.btnEnter.Text = "Tecla Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            this.btnEnter.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.form_KeyPress);
+            // 
+            // btnX
+            // 
+            this.btnX.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnX.Location = new System.Drawing.Point(12, 371);
+            this.btnX.Name = "btnX";
+            this.btnX.Size = new System.Drawing.Size(218, 67);
+            this.btnX.TabIndex = 3;
+            this.btnX.Text = "Tecla X";
+            this.btnX.UseVisualStyleBackColor = true;
+            this.btnX.Click += new System.EventHandler(this.btnX_Click);
+            this.btnX.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.form_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.btnX);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.form_KeyPress);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Button btnX;
+    }
+}
+
---
+++ TeclasComandos/TeclasComandos/TeclasComandos/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TeclasComandos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Select();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnX_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Clicou no botão Tecla X";
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Clicou no botão Tecla Enter";
+        }
+
+        private void form_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)97)
+            {
+                btnEnter.PerformClick();
+                label1.Text += " - Tecla a";
+            }
+        }
+    }
+}
